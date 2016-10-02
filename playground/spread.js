f591b74075d5e81b2e05f6ab47c2ce4aa51ccb76
yuan <field0917@genyatekiMacBook-Pro.local>
2016-10-01 18:04:55 -0700

    Add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,30 @@
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(add(3,1));
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+// var groupA = ['Chao', 'Ling'];
+// var groupB = ['Ting'];
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
+
+// var person = ['Chao', 27];
+// var personTwo = ['Yuan', 26];
+// //Hi Chao, you are 27
+// function greet(name, age) {
+//   console.log('Hi ' + name + ', you are ' + age);
+// }
+//
+// greet(...person);
+// greet(...personTwo);
+
+// var names = ['Mike', 'Ben'];
+// var final = ['Yuan', ...names];
+// //Hi Yuan
+// final.forEach(function (name) {
+//   console.log('Hi ' + name);
+// });
