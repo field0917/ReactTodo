c5bb3a986051e2633c736b8bb8251078c958c2ae
yuan <field0917@genyatekiMacBook-Pro.local>
2016-10-22 22:13:49 -0700

    Add actions for firebase login and logout

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,60 +1,24 @@
-var React = require('react');
-var uuid = require('node-uuid');
-var moment = require('moment');
+import React from 'react';
+import * as Redux from 'react-redux';
 
-//var TodoList = require('TodoList');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-// var TodoAPI = require('TodoAPI');
+import * as actions from 'actions';
 
-var TodoApp = React.createClass({
-  // getInitialState: function () {
-  //   return {
-  //     showCompleted: false,
-  //     searchText: '',
-  //     todos: TodoAPI.getTodos()
-  //   };
-  // },
-  // componentDidUpdate: function () {
-  //   TodoAPI.setTodos(this.state.todos);
-  // },
-  // handleAddTodo: function (text) {
-  //   this.setState({
-  //     todos: [
-  //       ...this.state.todos,
-  //       {
-  //         id: uuid(),
-  //         text: text,
-  //         completed: false,
-  //         createdAt: moment().unix(),
-  //         completedAt: undefined
-  //       }
-  //     ]
-  //   });
-  // },
-  // handleToggle: function (id) {
-  //   var updatedTodos = this.state.todos.map((todo) => {
-  //     if (todo.id === id) {
-  //       todo.completed = !todo.completed;
-  //       todo.completedAt = todo.completed ? moment().unix() : undefined;
-  //     }
-  //     return todo;
-  //   });
-  //   this.setState({todos: updatedTodos});
-  // },
-  // handleSearch: function (showCompleted, searchText) {
-  //   this.setState({
-  //     showCompleted: showCompleted,
-  //     searchText: searchText.toLowerCase()
-  //   });
-  // },
-  render: function () {
-    // var {todos, showCompleted, searchText} = this.state;
-    // var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+export var TodoApp = React.createClass({
+  onLogout(e) {
+    var {dispatch} = this.props;
+    e.preventDefault();
 
+    dispatch(actions.startLogout());
+  },
+  render() {
     return (
       <div>
+        <div className="page-actions">
+          <a href="#" onClick={this.onLogout}>Logout</a>
+        </div>
         <h1 className="page-title">Todo App</h1>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
@@ -70,4 +34,4 @@ var TodoApp = React.createClass({
   }
 });
 
-module.exports = TodoApp;
+export default Redux.connect()(TodoApp);
